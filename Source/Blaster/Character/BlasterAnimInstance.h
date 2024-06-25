19da1bb741012cc71549c03e945c7131c5448c3e
Moises Delgado <moises.f.delgado@gmail.com>
2024-06-25 16:40:36 -0400

    Adds crouching functionality

--- Source/Blaster/Character/BlasterAnimInstance.h
+++ Source/Blaster/Character/BlasterAnimInstance.h
@@ -32,4 +32,7 @@ private:
 
 	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
 	bool bWeaponEquipped;
+
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	bool bIsCrouched;
 };
